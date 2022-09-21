009754984d57bb970d0e052eed878ae23286b5c7
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-09-22 00:37:11 +0800

    [ex_2] 输入

---
+++ ex_2/src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+use ex_2::{parse_operator, Operator};
+
+pub fn ready_inputs() -> Vec<Operator> {
+    let lines = io::stdin().lines();
+    lines
+        .map(|l| parse_operator(&l.unwrap()).unwrap())
+        .collect()
+}
+
+fn main() {
+    let operators = ready_inputs();
+    println!("{:?}", operators);
+}
